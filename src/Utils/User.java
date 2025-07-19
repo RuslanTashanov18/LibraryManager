ae6f8d2cd3c82bde7e1fe11c208410d06565ae8f
RuslanTashanov18 <tashanov.2019@gmail.com>
2025-07-19 22:32:42 +0500

    Create Book and User Classes

---
+++ src/Utils/User.java
@@ -0,0 +1,16 @@
+package Utils;
+
+public class User {
+    protected int id;
+    protected String name;
+    public User(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+}
